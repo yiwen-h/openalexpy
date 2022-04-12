c42e06f661808035ab8a9473955eaadff59463cb
YiWen Hon <yiwench@gmail.com>
2022-04-12 15:40:26 +0100

    created create_df_from_doilist function

--- openalexpy/openalexpy.py
+++ openalexpy/openalexpy.py
@@ -5,6 +5,7 @@ which will be passed into a function
 
 import os
 import requests
+import pandas as pd
 
 
 # if using this package locally please set your own email address as an environmental variable
@@ -16,7 +17,6 @@ def works_query_from_doi(doi = "10.7861/clinmedicine.19-2-169", email = email_ad
     return url
 
 testurl = works_query_from_doi()
-testlist = ["10.7861/clinmedicine.19-2-169"]
 
 def send_request_to_openalex(url = testurl):
     try:
@@ -25,7 +25,25 @@ def send_request_to_openalex(url = testurl):
             metadata = response.json()
             return metadata
     except:
-        return f"Error with {url}"
+        return f"Error"
+
+testlist = ["10.7861/clinmedicine.19-2-169", "10.21037/cco.2018.11.03",
+            "10.1016/j.wneu.2018.11.176", "10.1002/14651858.CD006583.pub5"]
+
+def create_df_from_doilist(doilist = testlist):
+    metadata_list = []
+    error_list = []
+    for doi in doilist:
+        url = works_query_from_doi(doi = doi)
+        metadata = send_request_to_openalex(url=url)
+        if metadata != "Error":
+            metadata_list.append(metadata)
+        else:
+            error_list.append(doi)
+    df = pd.DataFrame(metadata_list)
+    print(error_list)
+    return df
 
 if __name__ == "__main__":
-    print(send_request_to_openalex())
+    df = create_df_from_doilist()
+    print(df.head())
